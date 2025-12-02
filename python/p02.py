eecd1186f5c376952f1e9991e1b0765ab182e896
Alex Coster <coster@google.com>
2025-12-02 09:57:19 +0100

    [day 02] Simplify the program a bit.

--- python/p02.py
+++ python/p02.py
@@ -1,40 +1,44 @@
 import re
-from typing import Iterator, List
+from typing import List
+from collections import namedtuple
 
 from absl import flags, app
 
 FLAGS = flags.FLAGS
-
 flags.DEFINE_string('input', '', 'Path to input file.')
 
-def generate_sequence(id_range: str, extended_rules: bool = False) -> Iterator[
-    int]:
+RangeResult = namedtuple('Result', ['simple_rule', 'extended_rules'])
+
+def solve_range(id_range: str) -> RangeResult:
     """
-    >>> [x for x in generate_sequence('11-22')]
-    [11, 22]
-    >>> [x for x in generate_sequence('1188511880-1188511890', True)]
-    [1188511885]
-    >>> [x for x in generate_sequence('95-115', True)]
-    [99, 111]
+    >>> solve_range('998-1012')
+    Result(simple_rule=1010, extended_rules=2009)
+    >>> solve_range('1188511880-1188511890')
+    Result(simple_rule=1188511885, extended_rules=1188511885)
     """
     assert id_range.count('-') == 1
     lower_bound, upper_bound = [int(x) for x in id_range.split('-')]
-    prog = re.compile(r"^([0-9]+)(\1)+$")
+    prog = re.compile(r"^([0-9]+)(\1)(\1)*$")
+
+    extended_total = 0
+    simple_total = 0
 
     for i in range(lower_bound, upper_bound + 1):
-        value = str(i)
-        length = len(value)
+        match = prog.match(str(i))
+        if match is None:
+            continue
 
-        if extended_rules:
-            if prog.fullmatch(value):
-                yield i
-        elif length % 2 == 0 and value[0:length // 2] == value[-(length // 2):]:
-            yield i
+        if match.group(3) is not None:
+            extended_total += i
+        else:
+            simple_total += i
+
+    return RangeResult(simple_total, extended_total + simple_total)
 
 
 def main(argv: List[str]) -> int:
     import doctest
-    doctest.testmod(raise_on_error=True)
+    doctest.testmod()
 
     if FLAGS.input != '':
         total = 0
@@ -42,8 +46,9 @@ def main(argv: List[str]) -> int:
         with open(FLAGS.input) as f:
             data = f.readline().strip()
             for id_range in data.split(','):
-                total += sum(generate_sequence(id_range))
-                extended_total += sum(generate_sequence(id_range, True))
+                result = solve_range(id_range)
+                total += result.simple_rule
+                extended_total += result.extended_rules
         print(
             f'Results: {total}, {extended_total}')
     return 0
