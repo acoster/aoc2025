ad0ff7494f4c8a539e767dbb79f9cf25e6fbc479
Alex Coster <coster@google.com>
2025-12-03 08:39:17 +0100

    Day 03, part 2.

--- python/p03.py
+++ python/p03.py
@@ -6,23 +6,33 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('input', '', 'Path to input file.')
 
 
-def find_joltage(bank: str) -> int:
+def find_joltage(bank: str, num_digits: int) -> int:
     """
-    >>> find_joltage('987654321111111')
+    >>> find_joltage('987654321111111', 2)
     98
-    >>> find_joltage('811111111111119')
+    >>> find_joltage('811111111111119', 2)
     89
-    >>> find_joltage('234234234234278')
+    >>> find_joltage('234234234234278', 2)
     78
+    >>> find_joltage('987654321111111', 12)
+    987654321111
     """
-    current_max = 0
+    result = 0
+    bank_length = len(bank)
+    idx_start = 0
 
-    for i in range(0, len(bank) - 1):
-        v = int(bank[i]) * 10 + int(max(bank[i + 1:]))
-        if v > current_max:
-            current_max = v
+    for i in range(num_digits):
+        # Don't search the last num_digits - i characters.
+        idx_end = bank_length - (num_digits - i - 1)
 
-    return current_max
+        # Find the index of the largest character in the search window.
+        idx_max = max(range(idx_start, idx_end), key=bank.__getitem__)
+
+        result = result * 10 + int(bank[idx_max])
+
+        idx_start = idx_max + 1
+
+    return result
 
 
 def main(argv: List[str]) -> int:
@@ -31,11 +41,15 @@ def main(argv: List[str]) -> int:
 
     if FLAGS.input != '':
         with open(FLAGS.input) as f:
-            total = 0
-            for line in f.readlines():
-                total += find_joltage(line.strip())
+            total_one = 0
+            total_two = 0
 
-            print(f'Total: {total}')
+            for line in f.readlines():
+                line = line.strip()
+                total_one += find_joltage(line, 2)
+                total_two += find_joltage(line, 12)
+
+            print(f'{total_one}, {total_two}')
 
 if __name__ == '__main__':
     app.run(main)
