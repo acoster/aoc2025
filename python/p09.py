59cf0a333b4bcbdf882416ef6e29f8a9f24888e5
Alex Coster <coster@google.com>
2025-12-10 08:31:49 +0100

    Day 9, part 2 (3.16s).

--- python/p09.py
+++ python/p09.py
@@ -1,8 +1,9 @@
-from dataclasses import dataclass
 from itertools import combinations
 from typing import List
 from answer import Answer
 from utils import Coord
+
+from shapely.geometry import Polygon
 
 
 def grid_area(a: Coord, b: Coord) -> int:
@@ -14,11 +15,22 @@ def grid_area(a: Coord, b: Coord) -> int:
 
 def solve(lines: List[str]) -> Answer:
     coords : List[Coord] = []
+
     for l in lines:
         a, b = [int(x) for x in l.split(',')]
         coords.append(Coord(x=a, y=b))
 
-    p1 = max([grid_area(a, b) for (a, b) in combinations(coords, 2)])
+    polygon = Polygon((a.x, a.y) for a in coords)
 
-    return Answer(p1, None)
+    p1 = 0
+    p2 = 0
+    for a, b in combinations(coords, 2):
+        area = grid_area(a, b)
+        p1 = max(p1, area)
+
+        sq = Polygon([[a.x, a.y], [b.x, a.y], [b.x, b.y], [a.x, b.y]])
+        if polygon.covers(sq):
+            p2 = max(p2, area)
+
+    return Answer(p1, p2)
 
